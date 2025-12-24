bc8d59591e4df86195d1daeb2db90262a22153da
daisy-bait <kadanarpa@gmail.com>
2025-12-24 13:53:54 -0500

    setup: product-service scaffold [SHOPMSVC-002]

---
+++ product-service/src/test/java/top/daisyflows/shoppingwithtechie/business/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.daisyflows.shoppingwithtechie.business.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
