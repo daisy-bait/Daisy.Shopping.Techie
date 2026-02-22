74787b737214145d1b6005019e6b05024a202807
daisy-bait <kadanarpa@gmail.com>
2026-02-22 01:44:48 -0500

    refactor: create final class to contains the reflection criteria builder utility isolated into a static function [SHOPMSVC-002]

--- product-service/src/main/java/top/daisyflows/shoppingwithtechie/business/product_service/service/ProductServiceImpl.java
+++ product-service/src/main/java/top/daisyflows/shoppingwithtechie/business/product_service/service/ProductServiceImpl.java
@@ -1,7 +1,6 @@
 package top.daisyflows.shoppingwithtechie.business.product_service.service;
 
 import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,11 +17,10 @@ import top.daisyflows.shoppingwithtechie.business.product_service.rest.dto.Produ
 import top.daisyflows.shoppingwithtechie.business.product_service.rest.dto.ProductToOutListDTO;
 import top.daisyflows.shoppingwithtechie.business.product_service.service.contracts.ProductServiceContract;
 
-import java.lang.reflect.Field;
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import static top.daisyflows.shoppingwithtechie.business.product_service.service.utils.ReflectionCriteriaBuilder.buildCriteriaFromDTO;
 
 @AllArgsConstructor
 @Service
@@ -32,13 +30,7 @@ public class ProductServiceImpl implements ProductServiceContract {
     private final ProductRepository productRepository;
     MongoTemplate complexProductRepository;
 
-    private static final String ILIKE = "i";
-    private static final String MIN = "min";
-    private static final String MAX = "max";
-    private static final String BLANK = "";
     private static final int INDEX_CRITERIA = 0;
-    private static final String STRING_CLASS = "String";
-    private static final String BIGDECIMAL_CLASS = "BigDecimal";
 
     public ProductToOutCreateDTO createProduct(ProductToInCreateDTO productRequest) {
         ProductDocument productDocument = ProductDocument.builder()
@@ -58,11 +50,12 @@ public class ProductServiceImpl implements ProductServiceContract {
         Query customSearch = new Query().with(pageable);
 
         List<Criteria> criteriaList = new ArrayList<>();
-        attachFields(criteriaList, ProductDocument.class, ProductToInListDTO.class, listRequest);
+        buildCriteriaFromDTO(criteriaList, ProductDocument.class, ProductToInListDTO.class, listRequest);
 
         log.info("=====[PRODUCT_SERVICE] INITIALIZING QUERY WITH PARAMS -------> {}", listRequest.toString());
 
-        if (!criteriaList.isEmpty()) customSearch.addCriteria(new Criteria().andOperator(criteriaList.toArray(new Criteria[INDEX_CRITERIA])));
+        if (!criteriaList.isEmpty())
+            customSearch.addCriteria(new Criteria().andOperator(criteriaList.toArray(new Criteria[INDEX_CRITERIA])));
 
         List<ProductDocument> productsDocument = complexProductRepository.find(customSearch, ProductDocument.class);
 
@@ -75,49 +68,6 @@ public class ProductServiceImpl implements ProductServiceContract {
         log.info("=====[PRODUCT_SERVICE] RESULT FROM PRODUCTS COLLECTION -------> {}", pageProductDocument.getContent());
 
         return pageProductDocument.map(res -> new ProductToOutListDTO(res.getId(), res.getName(), res.getDescription(), res.getPrice()));
-    }
-
-    @SneakyThrows
-    private void attachFields(List<Criteria> criteriaList, Class documentClass, Class dtoClass, Object listRequest) {
-
-        List<String> documentFields = Arrays.stream(documentClass.getDeclaredFields()).map(Field::getName).toList();
-        Field[] fields = dtoClass.getDeclaredFields();
-
-        for (Field field : fields) {
-            field.setAccessible(true);
-            if (field.get(listRequest) == null) continue;
-            log.info("=====[UTILITY_PREPARE_CRITERIA] Field Name: {}, Field Value: {}=====", field.getName(), field.get(listRequest));
-
-            switch (field.getType().getSimpleName()) {
-                case STRING_CLASS:
-                    logTypeField(STRING_CLASS);
-                    String fieldAsString = String.valueOf(field.get(listRequest));
-                    if (fieldAsString != null && !fieldAsString.isBlank() && documentFields.contains(field.getName()))
-                        criteriaList.add(Criteria.where(field.getName()).regex(fieldAsString, ILIKE));
-                    break;
-                case BIGDECIMAL_CLASS:
-                    logTypeField(BIGDECIMAL_CLASS);
-                    BigDecimal fieldAsNumber = (BigDecimal) field.get(listRequest);
-                    log.info(String.valueOf(fieldAsNumber));
-                    if (fieldAsNumber != null)
-                        if (field.getName().startsWith(MIN)) criteriaList.add(Criteria.where(
-                                lowerCaseFirstLetter(field.getName().replace(MIN, BLANK))
-                        ).gte(fieldAsNumber));
-                        else if (field.getName().startsWith(MAX)) criteriaList.add(Criteria.where(
-                                lowerCaseFirstLetter(field.getName().replace(MAX, BLANK))
-                        ).lte(fieldAsNumber));
-                        else criteriaList.add(Criteria.where(field.getName()).is(fieldAsNumber));
-                    break;
-            }
-        }
-    }
-
-    private String lowerCaseFirstLetter(String word) {
-        return word.substring(0, 1).toLowerCase() + word.substring(1);
-    }
-
-    private void logTypeField(String fieldType) {
-        log.info("=====[UTILITY_PREPARE_CRITERIA] Field is {} Class=====", fieldType);
     }
 
 }
---
+++ product-service/src/main/java/top/daisyflows/shoppingwithtechie/business/product_service/service/utils/ReflectionCriteriaBuilder.java
@@ -0,0 +1,72 @@
+package top.daisyflows.shoppingwithtechie.business.product_service.service.utils;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.query.Criteria;
+
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+public final class ReflectionCriteriaBuilder {
+
+    private static final String ILIKE = "i";
+    private static final String MIN = "min";
+    private static final String MAX = "max";
+    private static final String BLANK = "";
+    private static final Class<?> STRING_CLASS = String.class;
+    private static final Class<?> BIGDECIMAL_CLASS = BigDecimal.class;
+
+    @SneakyThrows
+    public static <D, T> void buildCriteriaFromDTO(
+            List<Criteria> criteriaList, Class<D> documentClass, Class<T> dtoClass, T listRequest
+    ) {
+
+        Set<String> documentFields = Arrays.stream(documentClass.getDeclaredFields()).map(Field::getName).collect(Collectors.toSet());
+        Field[] fields = dtoClass.getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object value = field.get(listRequest);
+            if (value == null) continue;
+
+            log.info("=====[UTILITY_PREPARE_CRITERIA] Field Name: {}, Field Value: {}=====", field.getName(), value);
+
+            Class<?> fieldType = field.getType();
+
+            if (fieldType.equals(STRING_CLASS)) {
+                logTypeField(STRING_CLASS.getSimpleName());
+                String fieldAsString = String.valueOf(value);
+                if (!fieldAsString.isBlank() && documentFields.contains(field.getName()))
+                    criteriaList.add(Criteria.where(field.getName()).regex(fieldAsString, ILIKE));
+            } else if (fieldType.equals(BIGDECIMAL_CLASS)) {
+                logTypeField(BIGDECIMAL_CLASS.getSimpleName());
+                BigDecimal fieldAsNumber = (BigDecimal) value;
+                log.info(String.valueOf(fieldAsNumber));
+
+                if (field.getName().startsWith(MIN)) criteriaList.add(Criteria.where(
+                        lowerCaseFirstLetter(field.getName().replace(MIN, BLANK))
+                ).gte(fieldAsNumber));
+                else if (field.getName().startsWith(MAX)) criteriaList.add(Criteria.where(
+                        lowerCaseFirstLetter(field.getName().replace(MAX, BLANK))
+                ).lte(fieldAsNumber));
+                else criteriaList.add(Criteria.where(field.getName()).is(fieldAsNumber));
+
+                break;
+            }
+        }
+    }
+
+    private static String lowerCaseFirstLetter(String word) {
+        return word.substring(0, 1).toLowerCase() + word.substring(1);
+    }
+
+    private static void logTypeField(String fieldType) {
+        log.info("=====[UTILITY_PREPARE_CRITERIA] Field is {} Class=====", fieldType);
+    }
+
+}
