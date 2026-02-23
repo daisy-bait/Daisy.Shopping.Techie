3897ea544986614be3a65482b27f2fcdf02fb574
daisy-bait <kadanarpa@gmail.com>
2026-02-23 01:47:35 -0500

    feat: add order-service template [SHOPMSVC-002]

---
+++ order-service/src/test/java/top/daisyflows/shoppingwithtechie/orders/business/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.daisyflows.shoppingwithtechie.orders.business.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
