8727679d5e4c554cba54f7aceae16b42a073090f
daisy-bait <kadanarpa@gmail.com>
2026-03-22 03:11:50 -0500

    feat: add shell for inventory service [SHOPMSVC-002]

---
+++ inventory-service/src/test/java/top/daisyflows/shoppingwithtechie/inventory/business/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.daisyflows.shoppingwithtechie.inventory.business.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
