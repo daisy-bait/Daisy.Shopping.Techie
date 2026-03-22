a6e9e35c3d8822caf34521a13fd8de3d172138ca
daisy-bait <kadanarpa@gmail.com>
2026-03-22 02:57:29 -0500

    feat: add order service in controller [SHOPMSVC-002]

--- order-service/src/main/java/top/daisyflows/shoppingwithtechie/orders/business/order_service/rest/controller/OrderController.java
+++ order-service/src/main/java/top/daisyflows/shoppingwithtechie/orders/business/order_service/rest/controller/OrderController.java
@@ -1,20 +1,24 @@
 package top.daisyflows.shoppingwithtechie.orders.business.order_service.rest.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import top.daisyflows.shoppingwithtechie.orders.business.order_service.rest.dto.OrderToInCreateDTO;
+import top.daisyflows.shoppingwithtechie.orders.business.order_service.rest.dto.OrderToOutCreateDTO;
+import top.daisyflows.shoppingwithtechie.orders.business.order_service.service.contracts.OrderServiceContract;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/products/v0/orders")
 public class OrderController {
 
+    private final OrderServiceContract orderService;
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public String placeOrder(@RequestBody OrderToInCreateDTO orderRequest) {
-        return "Order Placed Successfully";
+    public ResponseEntity<OrderToOutCreateDTO> placeOrder(@RequestBody OrderToInCreateDTO orderRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.placeOrder(orderRequest));
     }
 
 }
