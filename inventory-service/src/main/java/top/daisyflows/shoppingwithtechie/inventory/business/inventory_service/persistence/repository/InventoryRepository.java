e16df4a4e43694c15ff81be8f55b35aad807a4b6
daisy-bait <kadanarpa@gmail.com>
2026-03-23 00:16:39 -0500

    feat: add persistence repository layer for inventory service [SHOPMSVC-002]

---
+++ inventory-service/src/main/java/top/daisyflows/shoppingwithtechie/inventory/business/inventory_service/persistence/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package top.daisyflows.shoppingwithtechie.inventory.business.inventory_service.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import top.daisyflows.shoppingwithtechie.inventory.business.inventory_service.persistence.entity.InventoryEntity;
+
+public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
+
+}
