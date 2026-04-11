17e2475c3aab99e47d4dbbdf017338b149e73948
daisy-bait <kadanarpa@gmail.com>
2026-04-11 16:39:27 -0500

    feat(KAD-6): now extracts roles from jwt issuer provided from keycloak

--- api-gateway/src/main/java/top/daisyflows/shoppingwithtechie/gatekeeper/config/security/KeycloakAuthConverter.java
+++ api-gateway/src/main/java/top/daisyflows/shoppingwithtechie/gatekeeper/config/security/KeycloakAuthConverter.java
@@ -4,9 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @Slf4j
@@ -14,7 +19,18 @@ public class KeycloakAuthConverter implements Converter<Jwt, Collection<GrantedA
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt token) {
-        log.info(token.getClaims().get("realm_access").toString());
-        return null;
+        Map<String, Object> realmAccess = (Map<String, Object>) token.getClaims().get("realm_access");
+
+        if (realmAccess == null || !realmAccess.containsKey("roles")) {
+            return Collections.emptyList();
+        }
+
+        List<String> roles = (List<String>) realmAccess.get("roles");
+        Collection<GrantedAuthority> userRoles = new ArrayList<>();
+        roles.forEach(role -> {
+            userRoles.add(new SimpleGrantedAuthority("ROLE_".concat(role)));
+        });
+
+        return userRoles;
     }
 }
