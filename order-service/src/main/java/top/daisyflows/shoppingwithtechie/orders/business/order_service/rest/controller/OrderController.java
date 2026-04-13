2f0c2ce67eccbdbcfdef4b8b8b42e719ea283c70
daisy-bait <kadanarpa@gmail.com>
2026-04-13 01:15:46 -0500

    feat(KAD-6): implement circuit breaker timeout for slow calls

--- order-service/src/main/java/top/daisyflows/shoppingwithtechie/orders/business/order_service/rest/controller/OrderController.java
+++ order-service/src/main/java/top/daisyflows/shoppingwithtechie/orders/business/order_service/rest/controller/OrderController.java
@@ -1,12 +1,18 @@
 package top.daisyflows.shoppingwithtechie.orders.business.order_service.rest.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import top.daisyflows.shoppingwithtechie.error.ErrorMessage;
 import top.daisyflows.shoppingwithtechie.orders.business.order_service.rest.dto.OrderToInCreateDTO;
 import top.daisyflows.shoppingwithtechie.orders.business.order_service.rest.dto.OrderToOutCreateDTO;
 import top.daisyflows.shoppingwithtechie.orders.business.order_service.service.contracts.OrderServiceContract;
+
+import java.util.concurrent.CompletableFuture;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,10 +21,20 @@ public class OrderController {
 
     private final OrderServiceContract orderService;
 
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity<OrderToOutCreateDTO> placeOrder(@RequestBody OrderToInCreateDTO orderRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.placeOrder(orderRequest));
+    public ResponseEntity<CompletableFuture<OrderToOutCreateDTO>> placeOrder(@RequestBody OrderToInCreateDTO orderRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest))
+        );
+    }
+
+    public ResponseEntity<CompletableFuture<ErrorMessage>> fallbackMethod(OrderToInCreateDTO orderRequest, RuntimeException error) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(
+                CompletableFuture.supplyAsync(() -> new ErrorMessage("Service Unavailable An Error Ocurred: \"" + error.getMessage() + "\", Please try again in a few minutes", HttpStatus.SERVICE_UNAVAILABLE.value()))
+        );
     }
 
 }
---
+++ project-commons/src/main/java/top/daisyflows/shoppingwithtechie/error/ErrorMessage.java
@@ -0,0 +1,17 @@
+package top.daisyflows.shoppingwithtechie.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+
+    private String message;
+    private int statusCode;
+
+}
